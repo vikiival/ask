921d5799b2c21c0b3a1eae3fc46017a95afa8e67
yjhmelody <465402634@qq.com>
2021-02-26 18:57:06 +0800

    tmp

--- assembly/env/index.ts
+++ assembly/env/index.ts
@@ -1,6 +1,7 @@
-export * from './AccountType';
-export * from './BalanceType';
-export * from './BlockNumber';
-export * from './EmitableEvent';
-export * from './TimeStamp';
-export * from './Abi';
+export * from "./AccountType";
+export * from "./BalanceType";
+export * from "./BlockNumber";
+export * from "./EmitableEvent";
+export * from "./TimeStamp";
+export * from "./Abi";
+
